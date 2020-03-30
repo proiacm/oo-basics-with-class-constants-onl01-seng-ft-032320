b1fa3a2b51db657eeecf182b93c4d6528223d4bc
CiaraMaria Proia <proiacm@gmail.com>
2020-03-30 22:36:31 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,9 @@
+# learn spec/01_book_spec.rb
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+  
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -10,4 +13,8 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
 end
